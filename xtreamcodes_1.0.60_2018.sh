541fd9cf0115a8080934cae212044f4963f1b245
marconimp <marconimp@yahoo.com.br>
2018-06-03 09:23:50 -0300

    Update xtreamcodes_1.0.60_2018.sh

--- xtreamcodes_1.0.60_2018.sh
+++ xtreamcodes_1.0.60_2018.sh
@@ -19,16 +19,16 @@ echo -e "${jeshile} ┌───────────────────
 echo -e "${jeshile} │   Installing Web Server   │ \e[0m"
 echo -e "${jeshile} └───────────────────────────┘ \e[0m"
 echo " "
-apt-get update && apt-get upgrade -y && apt-get install htop -y
-apt-get install lsb-release nscd curl php5 php5-mysql php5-cli php5-curl unzip curl libcurl3 libcurl3-dev php5-curl -y
+sudo apt-get update && apt-get upgrade -y && apt-get install htop -y
+sudo apt-get install lsb-release nscd curl php5 php5-mysql php5-cli php5-curl unzip curl libcurl3 libcurl3-dev php5-curl -y
 sudo apt-get update && sudo apt-get install vlc vlc-plugin-* -y && sudo apt-get install vlc browser-plugin-vlc -y
 sudo wget https://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/youtube-dl -O /usr/local/bin/youtube-dl && chmod a+rx /usr/local/bin/youtube-dl
 sudo updatedb
 sudo apt-get update
 sudo apt-get install php5-dev php5-gd -y
-apt-get install php5-geoip php5-fpm mcrypt php5-mcrypt php5enmod mcrypt -y
-apt-get install apache2 apache2-mpm-prefork apache2-utils libapache2-mod-php5 libapr1 libaprutil1 libdbd-mysql-perl libdbi-perl libnet-daemon-perl libplrpc-perl libpq5 mysql-client-5.5 mysql-common mysql-server mysql-server-5.5 php5-common php5-mysql -y
-apt-get install phpmyadmin -y
+sudo apt-get install php5-geoip php5-fpm mcrypt php5-mcrypt php5enmod mcrypt -y
+sudo apt-get install apache2 apache2-mpm-prefork apache2-utils libapache2-mod-php5 libapr1 libaprutil1 libdbd-mysql-perl libdbi-perl libnet-daemon-perl libplrpc-perl libpq5 mysql-client-5.5 mysql-common mysql-server mysql-server-5.5 php5-common php5-mysql -y
+sudo apt-get install phpmyadmin -y
 sudo php5enmod mcrypt
 service apache2 reload && service apache2 restart
 echo " "
